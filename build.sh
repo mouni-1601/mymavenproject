1da7bbab32ed8721b84ae11593cf27208642c2ac
mouni-1601 <mounika-sai.ambati@capgemini.com>
2025-02-25 12:20:16 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Starting custom build script..."
+
+
+mvn clean install
+
+
+echo "Build completed. Running tests..."
+
+
+echo "Running custom post-build tasks..."
+
+
+
+
+echo "Build and test process completed!"
